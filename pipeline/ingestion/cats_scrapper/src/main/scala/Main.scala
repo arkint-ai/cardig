e64b1964edfab9404f6be292f992f90d3e9776f5
rodfer0x80 <rodrigolf080@gmail.com>
2024-09-13 00:04:28 +0100

    cars

--- pipeline/ingestion/cats_scrapper/src/main/scala/Main.scala
+++ pipeline/ingestion/cats_scrapper/src/main/scala/Main.scala
@@ -5,7 +5,9 @@ object Main extends IOApp.Simple {
   def scraper: IO[Unit] = {
     val bmwURL: String =
       "https://www.standvirtual.com/carros-novos/pesquisar/bmw/?OT=1"
-
+    !
+    // TODO:
+    // Why is this printing a List instead individual Strings?
     Scraper
       .scrapeProducts(bmwURL)
       .flatMap(productTitle => IO.println(productTitle))
